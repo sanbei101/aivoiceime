db2bd0c2ce4b6eea5d431dc45d5d85431e99db1b
sanbei101 <sanbei101@outlook.com>
2026-05-23 01:39:17 +0800

    Revert "使用阿里mirror"
    This reverts commit f2c32d17607f9a915615b4852c51b6bb44f0610b.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,5 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -20,8 +17,6 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
         google()
         mavenCentral()
     }
